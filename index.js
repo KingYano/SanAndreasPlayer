12ffd1a4139caa111d063bb70363626f8a4734a1
KingYano <belkadi.yanis.l@gmail.com>
2023-07-26 00:40:15 +0200

    correction bug javascript dans la progressbar

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const image = document.getElementById('cover'),
-title = document.getElementById('music-title'),
-artist = document.getElementById('music-artist'),
-currentTimeEl = document.getElementById('current-time'),
-durationEl = document.getElementById('duration'),
-progress = document.getElementById('progress'),
-playerProgress = document.getElementById('player-progress'),
-prevBtn = document.getElementById('prev'),
-nextBtn = document.getElementById('next'),
-playBtn = document.getElementById('play'),
-background = document.getElementById('bg-img');
+    title = document.getElementById('music-title'),
+    artist = document.getElementById('music-artist'),
+    currentTimeEl = document.getElementById('current-time'),
+    durationEl = document.getElementById('duration'),
+    progress = document.getElementById('progress'),
+    playerProgress = document.getElementById('player-progress'),
+    prevBtn = document.getElementById('prev'),
+    nextBtn = document.getElementById('next'),
+    playBtn = document.getElementById('play'),
+    background = document.getElementById('bg-img');
 
 
 const music = new Audio();
@@ -107,6 +107,6 @@ nextBtn.addEventListener('click', () => changeMusic(1));
 
 music.addEventListener('ended', () => changeMusic(1));
 music.addEventListener('timeupdate', updateProgressBar);
-music.addEventListener('click', setProgressBar);
+playerProgress.addEventListener('click', setProgressBar);
 
 loadMusic(songs[musicIndex]);
